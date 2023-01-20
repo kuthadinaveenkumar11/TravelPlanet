e87b5205b17903c6e02ea22eec080fd070959e92
Naveen Kumar <kuthadinaveenkumar11@gmail.com>
2023-01-21 00:08:39 +0530

    Git

---
+++ WEB_API/Models/ModelClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Empty_WEB_API.Models
+{
+    public class ModelClass
+    {
+        public int Emp_ID { get; set; }
+        public string Emp_Name { get; set; }
+        public Nullable<decimal> Number { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Blood_group { get; set; }
+    }
+}
